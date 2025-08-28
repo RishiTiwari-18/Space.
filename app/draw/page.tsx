4e77b90fe33c37566f11747fc4dfc55e7e1f6e14
rishiDatacode <rishi.datacode@gmail.com>
2025-08-28 19:42:25 +0530

    fixwed the Tl draw now it cover the full space of the screen

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -7,7 +7,7 @@ import 'tldraw/tldraw.css'
 export default function DrawPage() {
 	console.log(DefaultColorThemePalette)
 	return (
-		<div className="tldraw__editor" style={{ height: "91vh", width: "100vw" }}>
+		<div className="tldraw__editor" style={{ position: 'fixed', bottom:0, right:0, top:60, left:0, zIndex:0 }}>
 			<Tldraw />
 		</div>
 	)

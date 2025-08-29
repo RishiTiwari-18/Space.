90ecc899a11bcf6c2439580d7e17baa71e10168a
rishiDatacode <rishi.datacode@gmail.com>
2025-08-29 12:32:08 +0530

    Tldraw

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -7,7 +7,7 @@ import 'tldraw/tldraw.css'
 export default function DrawPage() {
 	console.log(DefaultColorThemePalette)
 	return (
-		<div className="tldraw__editor" style={{ position: 'fixed', bottom:0, right:0, top:60, left:0, zIndex:0 }}>
+		<div className="fixed top-16 bottom-0 right-0 left-0">
 			<Tldraw />
 		</div>
 	)

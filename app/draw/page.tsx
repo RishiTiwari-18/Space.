5b4a226d2bfc7afbc367f8af8866924159ad269c
RishiTiwari-18 <rishitiwari372@gmail.com>
2026-02-28 12:20:19 +0530

    chnaged env

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -9,7 +9,7 @@ export default function DrawPage() {
 	// const store = useSyncDemo({ roomId: '#kdsfbkjbkjsfjbdkjskjbd' })
 	return (
 		<div className="fixed top-16 bottom-0 right-0 left-0">
-			<Tldraw licenseKey={process.env.VITE_TLDRAW_LICENSE_KEY} persistenceKey="#sdfgkhndskfbnkdsb" />
+			<Tldraw licenseKey={process.env.NEXT_PUBLIC_TLDRAW_LICENSE_KEY} persistenceKey="#sdfgkhndskfbnkdsb" />
 		</div>
 	)
 }
